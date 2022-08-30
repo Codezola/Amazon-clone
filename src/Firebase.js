dddf931fae1ef4d51dc6229c2ad716e6c654a2e5
CodeZola <codezola@gmail.com>
2022-08-30 13:49:46 -0400

    first commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,12 +8,12 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCNUGB3mSaacHz0B990cFEi00qR1GJ7mOI",
-    authDomain: "e-clone-ad3b4.firebaseapp.com",
-    projectId: "e-clone-ad3b4",
-    storageBucket: "e-clone-ad3b4.appspot.com",
-    messagingSenderId: "948678916756",
-    appId: "1:948678916756:web:fc5d2a079bc6cb075fd281",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
     measurementId: "G-ZRZ27CS2MZ"
 };
 
